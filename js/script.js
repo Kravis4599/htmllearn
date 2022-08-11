c852d7ec02c49cde631f56d26542a2f71a5e0633
Kravis4599 <88815029+Kravis4599@users.noreply.github.com>
2022-08-11 18:12:50 +0600

    style

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: 'linear'
+});
+});
